2cf16cdad9f7b57e03d202996a3009f9b187475c
Luca <lucat1@users.noreply.github.com>
2020-07-11 17:04:38 +0200

    feat: kiwic command parsing

--- kiwic.c
+++ kiwic.c
@@ -5,18 +5,39 @@
 
 #include "kiwi.h"
 
-/* typedef struct { */
-/*   char *name; */
-/*   enum kiwic_cmds cmd; */
-/*   int config; // true or false */
-/*   int argc; */
-/* } command; */
+typedef struct {
+  char *name;
+  enum kiwic_cmds cmd;
+  int config; // true or false
+  int argc;
+} cmd;
 
 static Display *d; // display
 static Window r;   // root window
 
-int main(int argc, char *argv[]) {
+static cmd w_cmd = {"", KiwicLast, False, 0}; // wrong cmd; invalid
+static cmd cmds[] = {{"close", KiwicClose, False, 0},
+                     {"kill", KiwicKill, False, 0}};
+
+cmd get_cmd(char *name) {
+  int i;
+
+  for (i = 0; i < (int)(sizeof(cmds) / sizeof(cmd)); i++)
+    if (strcmp(cmds[i].name, name) == 0)
+      return cmds[i];
+
+  return w_cmd;
+}
+
+int main(int argc, char **argv) {
   XEvent ev;
+  cmd cmd;
+
+  if (argc < 2)
+    die("usage: kiwic <command> [opts]");
+
+  if ((cmd = get_cmd(argv[1])).cmd == w_cmd.cmd)
+    die("Invalid command %s", argv[1]);
 
   d = XOpenDisplay(NULL);
   if (!d)
@@ -30,8 +51,8 @@ int main(int argc, char *argv[]) {
   ev.xclient.message_type = XInternAtom(d, KIWI_CLIENT_EVENT, False);
   ev.xclient.format = 32;
 
-  // TODO: config and arg parsing
-  ev.xclient.data.l[0] = KiwicClose;
+  // TODO: config
+  ev.xclient.data.l[0] = cmd.cmd;
 
   XSendEvent(d, r, False, SubstructureRedirectMask, &ev);
   XSync(d, False);
